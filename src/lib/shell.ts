be7708e201a1b239660888422dd18a0e396750e8
Louis Dutton <clouisdutton@gmail.com>
2022-02-07 21:05:58 +0000

    converted shell to physics object

--- src/lib/shell.ts
+++ src/lib/shell.ts
@@ -1,24 +1,33 @@
+import { Body, Box, Vec3 } from "cannon-es";
 import * as THREE from "three";
+import { PhysicsObject } from "./physics";
 
 const SHELL_OFFSET = new THREE.Vector3(0, 1.3, 1.6);
 const SHELL_SPEED = 20;
 
-export const spawn = (tank: THREE.Mesh) => {
-  const shell = new THREE.Mesh(
+export const create = ({ mesh: tank }: PhysicsObject): PhysicsObject => {
+  const mesh = new THREE.Mesh(
     new THREE.BoxBufferGeometry(0.2, 0.2, 0.5),
     new THREE.MeshBasicMaterial({ color: 0xffd700 })
   );
 
-  shell.position.copy(tank.position);
-  shell.rotation.copy(tank.rotation);
-  shell.translateZ(SHELL_OFFSET.z);
-  shell.translateY(SHELL_OFFSET.y);
+  mesh.position.copy(tank.position);
+  mesh.rotation.copy(tank.rotation);
+  mesh.translateZ(SHELL_OFFSET.z);
+  mesh.translateY(SHELL_OFFSET.y);
 
-  return shell;
+  // physics
+  const mass = 5;
+  const shape = new Box(new Vec3(0.1, 0.1, 0.25));
+  const body = new Body({ mass, shape });
+  body.position.copy(mesh.position as unknown as Vec3);
+  body.applyImpulse(new Vec3(0, 0, 1));
+
+  return { mesh, body };
 };
 
-export const update = (shells: THREE.Mesh[], delta: number) => {
-  shells.forEach((shell) => {
-    shell.translateZ(SHELL_SPEED * delta);
-  });
-};
+// export const update = (shells: PhysicsObject[], delta: number) => {
+//   shells.forEach(({body}) => {
+//     body.(SHELL_SPEED * delta);
+//   });
+// };
